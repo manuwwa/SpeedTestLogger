a44f934da0eeec8ac27dc9a21ae1933f376a37a3
Marcin Packo <marp@netcompany.com>
2020-03-31 21:32:05 +0200

    Implementation

--- sensorSpeedTestLogger.py
+++ sensorSpeedTestLogger.py
@@ -1,1 +1,36 @@
+import speedtest
+import json
+import mysql.connector
+
+threads = None
+with open('config.JSON') as config_file:
+    config = json.load(config_file)
+data = {}
+s = speedtest.Speedtest()
+
+#Read config
+database = config['database']
+table = config['table']
+table = config['table']
+user = config['user']
+password = config['password']
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user=user,
+  passwd=password,
+  database=database
+)
+
+#Read data
+download = s.results.download
+upload = s.results.upload
+ping = s.results.ping
+
+mycursor = mydb.cursor()
+
+sql = "INSERT INTO " +table+ " (ping, upload, download, connectionTimeOut) VALUES (%s, %s, %s)"
+val = (ping, upload, download,False)
+mycursor.execute(sql, val)
+
 
